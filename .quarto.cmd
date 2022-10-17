50de6e8fb64e619ba657d8f1f9bbf608e4975c70
FONDECYTACC <57901662+FONDECYTACC@users.noreply.github.com>
2022-10-17 11:23:23 -0300

    corr3: cc, p83, step 1 and 2

--- .quarto.cmd
+++
@@ -1,4 +0,0 @@
-      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
-     1096       1    1096      19432  cons1     197609 19:59:16 /usr/bin/bash
-     1153    1096    1153      25052  cons1     197609 20:05:48 /usr/bin/PS
-      849       1     849      27916  cons0     197609 14:48:23 /usr/bin/bash
